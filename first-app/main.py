25889d2d4351b93159e3a452b80a41c63cae7388
DEVENDRA-5470 <ysdevn@gmail.com>
2026-08-06 12:07:24 +0530

    update

--- first-app/main.py
+++ first-app/main.py
@@ -4,6 +4,7 @@ from sqlalchemy import text
 import requests
 from database import SessionLocal,engine,Base
 import models , schemas
+import json
 
 Base.metadata.create_all(bind=engine)
 
@@ -33,4 +34,46 @@ def get_product():
     resp=requests.get(f"https://dummyjson.com/products")
     data=resp.json()
     return data
+
+@app.post("/create_staff")
+def create(staff:schemas.StaffCreate , db:Session = Depends(get_db)):
+    new_staff=models.Staff(emp_name=staff.emp_name,emp_age=staff.emp_age,emp_city=staff.emp_city)
+    db.add(new_staff)
+    db.commit()
+    db.refresh(new_staff)
+    return new_staff
+
+
+@app.get("/staff")
+def get_all_staff(db:Session = Depends(get_db)):
+    staff=db.query(models.Staff).all() # select * from staff
+    return staff
+
+@app.get("/staff/{staff_id}")
+def get_all_staff(staff_id:int ,db:Session = Depends(get_db)):
+    staff=db.query(models.Staff).filter(models.Staff.id==staff_id).first()
+    return staff
+
+@app.post("/staff/bulk-upload")
+def bulk_upload_staff(db:Session=Depends(get_db)):
+    try:
+        with open("staff_data.json","r") as f:
+            staff_list=json.load(f)
+    except Exception as e:
+        raise HTTPException(status_code=400 , detail=f" Error {str(e)}")
+
+    created=[]
+    for item in staff_list:
+        new_staff=models.Staff(
+            emp_name=item["emp_name"],
+            emp_age=item["emp_age"],
+            emp_city=item["emp_city"],
+        )
+        db.add(new_staff)
+        created.append(new_staff)
+    db.commit()
+    for staff in created:
+        db.refresh(staff)
+    return {"Inserted ":len(created),"staff":created}
+
 
