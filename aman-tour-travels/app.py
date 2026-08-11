e74f692a5622afa60f1aaecce4fa4b41ddb8de07
DEVENDRA-5470 <ysdevn@gmail.com>
2026-08-11 15:33:25 +0530

    updated

--- aman-tour-travels/app.py
+++ aman-tour-travels/app.py
@@ -1,7 +1,20 @@
 from flask import Flask,render_template
+import os
+import requests
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app=Flask(__name__)
+
+app.secret_key=os.getenv("SECRET_KEY","this-is-secure-token-123456")
+
+FASTAPI_URL=os.getenv("FASTAPI_URL")
+
+@app.route("/health")
+def backend_health():
+    resp=requests.get(FASTAPI_URL+"health")
+    return "Connected To backend ✅"
 
 @app.route("/")
 def home():
